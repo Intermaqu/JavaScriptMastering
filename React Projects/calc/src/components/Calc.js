4f6f79bbd5978d4231f1da8777f27d573c845b7d
intermaqu <intermaqu@gmail.com>
2022-06-23 00:57:15 +0200

    React Calc Component

---
+++ React Projects/calc/src/components/Calc.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+
+const Calc = () => {
+    const [state, setState] = React.useState("");
+    const [display, setDisplay] = React.useState("");
+
+    return (
+        <div>
+            Calc
+        </div>
+    )
+}
+
+export default Calc
