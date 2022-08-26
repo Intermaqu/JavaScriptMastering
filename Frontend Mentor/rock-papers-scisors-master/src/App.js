c9461a548c5a0fccaf04994d938ac432481a520e
Intermaqu <intermaqu@gmail.com>
2022-08-26 11:56:15 +0200

    Frontend Master: rock-paper-scissors

--- Frontend Mentor/rock-papers-scisors-master/src/App.js
+++ Frontend Mentor/rock-papers-scisors-master/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles/app.css";
+import "./styles/general.css";
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app--score">
+        <div>
+          <p className="app--score--pick">Rock</p>
+          <p className="app--score--pick">Paper</p>
+          <p className="app--score--pick">Scissors</p>
+        </div>
+        <div className="app--score--score">
+          <p>score</p>
+          <h1>{currentScore}</h1>
+        </div>
+      </div>
+      <div className="app--picks">
+        <div className="pick-container paper-container">
+          <div className="pick-image-container">
+            <img src="./images/icon-paper.svg" className="paper-icon" />
+          </div>
+        </div>
+        <div className="pick-container scissors-container">
+          <div className="pick-image-container">
+            <img src="./images/icon-scissors.svg" className="scissors-icon" />
+          </div>
+        </div>
+        <div className="pick-container rock-container">
+          <div className="pick-image-container">
+            <img src="./images/icon-rock.svg" className="rock-icon" />
+          </div>
+        </div>
+      </div>
+      <div className="app--rules-container">
+        <p>Rules</p>
+      </div>
     </div>
   );
 }
