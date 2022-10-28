c4f0346851d40e45cb9329deb0eac546bef489ab
Intermaqu <intermaqu@gmail.com>
2022-10-28 10:04:05 +0200

    SBD2 Auth

--- React Projects/sbd2_shop/src/components/Authentication.js
+++ React Projects/sbd2_shop/src/components/Authentication.js
@@ -5,6 +5,22 @@ import { TextField, MenuItem, Select, InputLabel } from "@mui/material";
 const Authentication = () => {
   const [isLoginShown, setIsLoginShown] = useState(true);
   const [genter, setGender] = useState("");
+  const [userData, setUserData] = useState({
+    name: "",
+    surname: "",
+    gender: "",
+    email: "",
+    password: "",
+    country: "",
+    city: "",
+    street: "",
+    apartmentNum: "",
+    postCode: "",
+  });
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
   const handleGender = (e) => {
     setGender(e.target.value);
@@ -14,6 +30,14 @@ const Authentication = () => {
   };
   const setLogin = () => {
     setIsLoginShown(true);
+  };
+
+  const handleChangeRegister = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleChangeLogin = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
   };
 
   return (
@@ -35,34 +59,51 @@ const Authentication = () => {
       </div>
       {isLoginShown ? (
         <div className="authentication-login">
-          <TextField label="Email" variant="standard" />
+          <TextField
+            label="Email"
+            variant="standard"
+            name="email"
+            value={loginData.email}
+            onChange={handleChangeLogin}
+          />
           <TextField
             id="outlined-password-input"
+            name="password"
+            onChange={handleChangeLogin}
             label="Password"
             type="password"
+            value={loginData.password}
             autoComplete="current-password"
             variant="standard"
           />
-          <button className="authentication-login-button">SIGN IN</button>
+          <button className="authentication-login-button" onClick={()=>console.log(loginData)}>SIGN IN</button>
         </div>
       ) : (
         <div className="authentication-register">
           <TextField
+            name="name"
+            onChange={handleChangeRegister}
+            value={userData.name}
             label="Name"
             variant="standard"
             className="grid-l"
             style={{ gridRow: "1/2" }}
           />
           <TextField
+            name="surname"
+            onChange={handleChangeRegister}
+            value={userData.surname}
             label="Surname"
             variant="standard"
             className="grid-l"
             style={{ gridRow: "2/3" }}
           />
           <TextField
+            name="gender"
+            onChange={handleChangeRegister}
+            value={userData.gender ? userData.gender : ""}
             id="select"
             label="Gender"
-            value=""
             select
             variant="standard"
             className="grid-l"
@@ -73,13 +114,18 @@ const Authentication = () => {
             <MenuItem value="Other">Other</MenuItem>
           </TextField>
           <TextField
+            name="email"
+            onChange={handleChangeRegister}
+            value={userData.email}
             label="Email"
             variant="standard"
             className="grid-l"
             style={{ gridRow: "4/5" }}
           />
           <TextField
-            id="outlined-password-input"
+            name="password"
+            onChange={handleChangeRegister}
+            value={userData.password}
             label="Password"
             type="password"
             autoComplete="current-password"
@@ -88,30 +134,45 @@ const Authentication = () => {
             style={{ gridRow: "5/6" }}
           />
           <TextField
+            name="country"
+            onChange={handleChangeRegister}
+            value={userData.country}
             label="Country"
             variant="standard"
             className="grid-r"
             style={{ gridRow: "1/2" }}
           />
           <TextField
+            name="city"
+            onChange={handleChangeRegister}
+            value={userData.city}
             label="City"
             variant="standard"
             className="grid-r"
             style={{ gridRow: "2/3" }}
           />
           <TextField
+            name="street"
+            onChange={handleChangeRegister}
+            value={userData.street}
             label="Street"
             variant="standard"
             className="grid-r"
             style={{ gridRow: "3/4" }}
           />
           <TextField
+            name="apartmentNum"
+            onChange={handleChangeRegister}
+            value={userData.apartmentNum}
             label="Apartment / Suite"
             variant="standard"
             className="grid-r"
             style={{ gridRow: "4/5" }}
           />
           <TextField
+            name="postCode"
+            onChange={handleChangeRegister}
+            value={userData.postCode}
             label="Postal Code"
             variant="standard"
             className="grid-r"
