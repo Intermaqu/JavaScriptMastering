e5bbd7d08675c75611658205e5b7a6a116b4566b
Intermaqu <intermaqu@gmail.com>
2023-03-06 16:34:23 +0100

    AKAI - riddles

---
+++ AKAI - Coding Challenge/riddles/index.js
@@ -0,0 +1,18 @@
+// 1. odwróć liczbę
+// np dla 12345, funkcja powinna zwrócić 54321
+function reverseNumber(number) {
+  return parseInt(number.toString().split("").reverse().join(""));
+}
+
+console.log("1.", reverseNumber(12345));
+
+// 2. doodaj do siebie wszystkie wartości z tablicy, które są parzyste
+// dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
+const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function addEven(array) {
+  return array.reduce((acc, curr) => (curr % 2 == 0 ? acc + curr: acc ), 0);
+}
+
+console.log("2.", addEven(tab));
+
+// run: node index.js 
