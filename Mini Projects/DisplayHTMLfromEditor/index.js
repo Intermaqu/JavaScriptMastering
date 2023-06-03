2a1e943c33f80c587390c3a4a7374dfc3998f34a
Intermaqu <70236348+Intermaqu@users.noreply.github.com>
2023-06-03 20:49:12 +0200

    First step to cssbattle clone

--- Mini Projects/DisplayHTMLfromEditor/index.js
+++ Mini Projects/DisplayHTMLfromEditor/index.js
@@ -1,6 +1,24 @@
+// import html2canvas from "./node_modules/html2canvas/dist/html2canvas.js";
+
+import html2canvas from "html2canvas";
+
 const textarea = document.querySelector("textarea");
 const iframe = document.querySelector("iframe");
 textarea.addEventListener("input", (event) => {
     iframe.srcdoc = textarea.value;
     console.log(textarea.value);
+});
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+        e.preventDefault();
+        // textarea.value += "    ";
+    }
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    html2canvas(iframe).then((canvas) => {
+        document.body.appendChild(canvas);
+    });
 });
